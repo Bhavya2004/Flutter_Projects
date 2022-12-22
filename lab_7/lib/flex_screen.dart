59a624a8af9a4f4d3e636b31e3edfe2a16573d9d
Bhavya Barai <bhavyabarai85@gmail.com>
2022-12-22 19:43:04 +0530

    lab-7 modified

--- lab_7/lib/flex_screen.dart
+++ lab_7/lib/flex_screen.dart
@@ -1,83 +1,141 @@
 import 'package:flutter/material.dart';
 
-class FlexScreen extends StatelessWidget{
+class FlexScreen extends StatelessWidget {
+  var text;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  Expanded(
-                    child: Container(
-                      color: Colors.yellow,
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.yellow,
+                  child: Center(
+                    child: Text(
+                      'Yellow',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                      textAlign: TextAlign.center,
                     ),
-
                   ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.black,
-                    ),
-
-                  ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.purple,
-                    ),
-
-                  ),
-                ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  Expanded(
-                      child: Container(
-                        color: Colors.blue,
+              Expanded(
+                child: Container(
+                  color: Colors.black,
+                  child: Center(
+                    child: Text(
+                      'Black',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.purple,
+                  child: Center(
+                    child: Text(
+                      'Purple',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                  child: Container(
+                    color: Colors.blue,
+                    child: Center(
+                      child: Text(
+                        'Blue',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                        textAlign: TextAlign.center,
                       ),
-                      flex:3
-                  ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.lightGreenAccent,
-                    ),
-                    flex: 2,
-                  ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.pink,
                     ),
                   ),
-                ],
+                  flex: 3),
+              Expanded(
+                child: Container(
+                  color: Colors.lightGreenAccent,
+                  child: Center(
+                    child: Text(
+                      'Light Green',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                flex: 2,
               ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  Expanded(
-                    child: Container(
-                      color: Colors.green,
+              Expanded(
+                child: Container(
+                  color: Colors.pink,
+                  child: Center(
+                    child: Text(
+                      'Pink',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.red,
-                    ),
-                    flex: 2,
-                  ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.tealAccent,
-                    ),
-
-                  ),
-                ],
+                ),
               ),
-            )
-          ],
-        ));
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  child: Center(
+                    child: Text(
+                      'Green',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  child: Center(
+                    child: Text('Red',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                      textAlign: TextAlign.center,),
+                  ),
+                ),
+                flex: 2,
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.tealAccent,
+                  child: Center(
+                    child: Text('Teal',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                      textAlign: TextAlign.center,),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    ));
   }
-
 }
